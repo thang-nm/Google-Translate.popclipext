041e7868d28d66f0ef49b914413a509834956426
thang-nm <nm.thang@outlook.com>
2020-01-12 20:13:44 +0700

    Update config tl

--- Google-Trans.popclipext/google-trans.php
+++ Google-Trans.popclipext/google-trans.php
@@ -1,33 +1,53 @@
 <?php
 
-function googleTrans($q) {
-    $query = array(
-      "client" => "gtx",
-      "sl" => "auto",
-      "tl" => "vi",
-      "dt" => "t",
-      "q" => $q,
-    );
-    $ch = curl_init();
-    $url = 'https://translate.googleapis.com/translate_a/single?';
+function option($key, $deault = '') {
+  $tl = getenv($key);
+  if (empty($tl)) {
+    return $default;
+  }
+  $start = strpos($tl, '(');
+  $end = strpos($tl, ')');
+  if ($start === false || $end === false) {
+    return $default;
+  }
+  return  substr($tl, $start + 1, $end - $start - 1);
+}
 
-    curl_setopt($ch, CURLOPT_URL, $url . http_build_query($query));
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
-    $response = curl_exec($ch);
-    curl_close($ch);
+function normalize($q) {
+  return method_exists('Normalizer', 'normalize')
+    ? Normalizer::normalize($q)
+    : $q;
+}
 
-    if (!$response) {
-      die('');
-    }
+function googleTrans($q, $tl) {
+  $query = array(
+    "client" => "gtx",
+    "sl" => "auto",
+    "tl" => $tl,
+    "dt" => "t",
+    "q" => normalize($q),
+  );
+  $ch = curl_init();
+  $url = 'https://translate.googleapis.com/translate_a/single?';
 
-    $json = @json_decode($response, true);
-    return isset($json[0][0][0]) ? $json[0][0][0] : '';
+  curl_setopt($ch, CURLOPT_URL, $url . http_build_query($query));
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+  // curl_setopt($ch, CURLOPT_PROXY, '127.0.0.1:9090');
+  $response = curl_exec($ch);
+  curl_close($ch);
+
+  if (!$response) {
+    die('');
+  }
+
+  $json = @json_decode($response, true);
+  return isset($json[0][0][0]) ? $json[0][0][0] : '';
 }
 
 if ($input = getenv('POPCLIP_TEXT')) {
-    echo googleTrans($input);
+  echo googleTrans($input, option('POPCLIP_OPTION_TL', 'en'));
 }
 ?>
