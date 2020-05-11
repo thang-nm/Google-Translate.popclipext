726befcb51ad7234a68dca3f5d4b00e66814330d
thang-nm <nm.thang@outlook.com>
2020-05-11 11:37:49 +0700

    Test with premium key

--- Google-Translate.popclipext/google-translate.php
+++ Google-Translate.popclipext/google-translate.php
@@ -10,7 +10,7 @@ function option($key, $deault = '') {
   if ($start === false || $end === false) {
     return $default;
   }
-  return  substr($tl, $start + 1, $end - $start - 1);
+  return substr($tl, $start + 1, $end - $start - 1);
 }
 
 function normalize($q) {
@@ -26,20 +26,21 @@ function normalize($q) {
 
 function googleTrans($q, $tl) {
   $query = array(
-    "client" => "gtx",
-    "sl" => "auto",
-    "tl" => $tl,
-    "dt" => "t",
+    "key" => "",
+    "format" => "text",
+    "target" => $tl,
     "q" => normalize($q),
   );
   $ch = curl_init();
-  $url = 'https://translate.googleapis.com/translate_a/single?';
+  $url = 'https://translation.googleapis.com/language/translate/v2?';
 
   curl_setopt($ch, CURLOPT_URL, $url . http_build_query($query));
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 6);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'X-Ios-Bundle-Identifier: x',
+  ]);
   // curl_setopt($ch, CURLOPT_PROXY, '127.0.0.1:9090');
   $response = curl_exec($ch);
   curl_close($ch);
@@ -55,15 +56,10 @@ if ($input = getenv('POPCLIP_TEXT')) {
   $modifier = getenv('POPCLIP_MODIFIER_FLAGS');
   $tlKey = (int)$modifier === 1048576 ? 'POPCLIP_OPTION_TLC' : 'POPCLIP_OPTION_TL';
   $json = googleTrans($input, option($tlKey, 'en'));
-  if (!isset($json[0])) {
+  if (!isset($json['data']['translations'][0])) {
     die('');
   }
-  
-  echo array_reduce($json[0], function($carry, $item) {
-    if (isset($item[0])) {
-      $carry .= $item[0];
-      return $carry;
-    }
-  }, '');
+
+  echo trim(preg_replace('/\s\s+/', ' ', $json['data']['translations'][0]['translatedText']));
 }
 ?>
